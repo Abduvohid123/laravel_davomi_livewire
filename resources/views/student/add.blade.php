55f1a9f1fb8144d0444e8af591f5082ebafbfc7c
Abduvohid Mirzamahmudov <abduvohid.mirzamahmudov@gmail.com>
2021-05-15 22:30:39 +0500

    sweetalert

--- resources/views/student/add.blade.php
+++ resources/views/student/add.blade.php
@@ -8,6 +8,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -61,6 +63,19 @@
 
     }
 </script>
+@if (\Illuminate\Support\Facades\Session::has('ok'))
+    <script>
+        toastr.success(" {!! \Illuminate\Support\Facades\Session::get('ok') !!} ");
+    </script>
+@endif
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
+@if (\Illuminate\Support\Facades\Session::has('ok'))
+    <script>
+        swal('Greet job'," {!! \Illuminate\Support\Facades\Session::get('ok') !!} ",'success',{
+            button:'OK',
+        })
+    </script>
+@endif
 </body>
 </html>
 
