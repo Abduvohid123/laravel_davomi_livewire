4fdcede0f70e2ba183ff93b3f87a7c11baf0c18f
Abduvohid123 <abduvohid.mirzamahmudov@gmal.com>
2021-06-09 15:22:26 +0500

    multiple connection

--- app/Http/Controllers/EloquentController.php
+++ app/Http/Controllers/EloquentController.php
@@ -2,22 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MultipleConnection;
 use App\Models\User;
+
 class EloquentController extends Controller
+
+
 {
-    public function helper(){
+
+    public function multiple_connection()
+    {
+        return MultipleConnection::all();
+
+    }
+
+    public function helper()
+    {
         return split_name('salom qalesan ');
     }
 
     public function first()
     {
-        $user=User::where('id',1)->get();//massiv qaytardi
+        $user = User::where('id', 1)->get();//massiv qaytardi
         return $user;
     }
 
     public function find()
     {
-        $user=User::where('id',1)->first();
+        $user = User::where('id', 1)->first();
         return $user;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('multiple', [\App\Http\Controllers\EloquentController::class, 'multiple_connection']);
 Route::get('scroll', [\App\Http\Controllers\ScrollPaginationController::class, 'index']);
 
 
